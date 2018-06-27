b5744e136a97f557be3bf14575c615afa0843a63
Malind Kowalski <malind.nikulski@gmail.com>
2018-06-26 18:59:42 -0600

    change function to ES6

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce some HTML
-const App = function() {
+const App = () => {
   return <div>Hello!</div>
 }
 
 // Take this component's generated HTML and put it on the page(DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
-//reactDOM takes 2nd argument and that argument is a reference to an existing DOM node on page
