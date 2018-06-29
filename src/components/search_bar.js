d7045af21ffc6f09750b36ef5d149172993c7417
Malind Kowalski <malind.nikulski@gmail.com>
2018-06-29 14:10:23 -0600

    remove notes, modify react.component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-//Functional component: 1 function, some info goes in and JSX comes out
-// const SearchBar = () => {
-  // return <input />;
-// }
-
-//Class component has some type of internal record keeping, to be aware of itself since it's been rendered
-//Create class-based component with ES6
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render() {
     return <input />;
   }
