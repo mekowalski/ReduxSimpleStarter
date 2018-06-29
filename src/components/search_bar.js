86a7e9dcc3301e2975ed5069ab65211e54c460ea
Malind Kowalski <malind.nikulski@gmail.com>
2018-06-29 14:05:55 -0600

    modify functional comp to class comp

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />;
+//Functional component: 1 function, some info goes in and JSX comes out
+// const SearchBar = () => {
+  // return <input />;
+// }
+
+//Class component has some type of internal record keeping, to be aware of itself since it's been rendered
+//Create class-based component with ES6
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
 }
 
 export default SearchBar;
