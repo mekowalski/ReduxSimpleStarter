29f8030cd94714d9ab85224891fde98b07f9bd16
Malind Kowalski <malind.nikulski@gmail.com>
2018-06-29 15:05:33 -0600

    create state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  //define state, only class-based comps have state
+  //constructor is the 1st & only function called automatically whenever a new class instance is created
+  constructor(props) {
+    //passes on parent method via super
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange={(event) => console.log(event.target.value)} />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 }
 
