a4317006168d69a4d054dcf69732eaedf07dba0e
Malind Kowalski <malind.nikulski@gmail.com>
2018-06-29 14:46:50 -0600

    create event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <input />;
+    return <input onChange={this.onInputChange} />;
+  }
+
+  //create event handler
+  onInputChange(event) {
+    console.log(event.target.value)
   }
 }
 
