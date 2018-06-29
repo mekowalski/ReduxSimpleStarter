23b74ae12ec6604de98b1ebbd94d12f307494348
Malind Kowalski <malind.nikulski@gmail.com>
2018-06-29 13:46:31 -0600

    created SearchBar component, exported and imported, nest under App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 
 //variable to hold API key
 const API_KEY = 'AIzaSyCoQ10LAtQjQg1CAjkpIYhE1J_-JrgWVaE';
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>Hello!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 // Take this component's generated HTML and put it on the page(DOM)
