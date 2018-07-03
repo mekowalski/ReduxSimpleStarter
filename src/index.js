d5287fab2e239bb8330ac8996f80cb37093292ac
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-03 11:53:58 -0600

    refactor functional to class-based component, create constructor and state

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 
-//variable to hold API key
 const API_KEY = 'AIzaSyCoQ10LAtQjQg1CAjkpIYhE1J_-JrgWVaE';
 
-//example
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data)
-})
-
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+// refactor from functional component to class-based component to utilize state and data persist
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+      //this.setState({ videos: videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it on the page(DOM)
