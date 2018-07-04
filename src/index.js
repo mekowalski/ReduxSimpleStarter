b5208b12fa244a2497116d5a08c1fedb040fb642
Malind Kowalski <malind.nikulski@gmail.com>
2018-07-04 14:46:12 -0600

    create video selection status

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@ import VideoDetail from './components/video_detail'
 const API_KEY = 'AIzaSyCoQ10LAtQjQg1CAjkpIYhE1J_-JrgWVaE';
 
 // Create a new component. This component should produce some HTML
+// add concept of selected video, video object always passed into video detail
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
@@ -23,7 +30,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
+        <VideoDetail video={this.state.selectedVideo} /> //now passing selected video into video detail
         <VideoList videos={this.state.videos} />
       </div>
     )
